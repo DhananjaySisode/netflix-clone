077892574eeb669434a65c432239f5da7602f6a3
DhananjaySisode <dhananjay_sisode@hotmail.com>
2021-02-11 19:34:48 +0530

    nav bar added

---
+++ src/Nav.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, setshow] = useState(false);
+
+  const transistionForNavBar = () => {
+    if (window.scrollY > 100) {
+      setshow(true);
+    } else {
+      setshow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transistionForNavBar);
+    return () => window.removeEventListener("scroll", transistionForNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCWc0RCHWtlD0mh8EANmdLCSWeFbYE81XQ",
+  authDomain: "netflix-clone-8d38e.firebaseapp.com",
+  projectId: "netflix-clone-8d38e",
+  storageBucket: "netflix-clone-8d38e.appspot.com",
+  messagingSenderId: "915767860241",
+  appId: "1:915767860241:web:6b9175dd9aa1ca400ae5b0",
+};
